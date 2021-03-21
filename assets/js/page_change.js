2b0d038c16adfa049e32d4d91ba9a79bec593ee7
nikita-traynin <nikitatraynin@gmail.com>
2021-03-21 07:13:33 -0500

    prettified page_change.js

--- assets/js/page_change.js
+++ assets/js/page_change.js
@@ -1,36 +1,7 @@
-console.log("yoyoyo");
+$.get("/home/", function(data) {
+    $("nav").after(data);
+});
 
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "/home/");
-xhr.onreadystatechange = function (data) {
-    if(xhr.readyState == 4){
-      var status = xhr.status
-      if(status === 0 || (status >= 200 && status < 400)){
-        $("nav").after(xhr.response);
-      }else{
-        console.log('Error - XMLHttpRequest status: '+xhr.status);
-      }
-    }
-  };
-xhr.send();
-
-//var request = $.ajax({
-//    url: "/home.html",
-//    method: "GET",
-//    dataType: "html",
-//    success: function(data, textStatus, jqXHR) {
-//
-//    },
-//    error: function(jqXHR, textStatus, errorThrown) {
-//        console.log("Text Status: " + textStatus);
-//        console.log("Error Thrown: " + errorThrown);
-//        if(jqXHR.status==404) {
-//            alert(errorThrown);
-//        }
-//    }
-//});
-
-console.log("exiting page change script");
 
 
 
