cfe7edab3b7bc696d451b77753a4f680226c5e61
nikita-traynin <nikitatraynin@gmail.com>
2021-03-21 11:19:30 -0500

    SPA except for home buttons and nav

--- assets/js/page_change.js
+++ assets/js/page_change.js
@@ -1,10 +1,26 @@
-
-
-function openPage(page_name) {
+function loadPage(page_name) {
+    if(page_name == "") {
+        page_name = "home";
+    }
     $.get("/" + page_name + "/", function(data) {
+        //remove the previous content
         $("#page-wrapper").remove();
+
+        //add the current content
         $("nav").after(data);
     })
+}
+
+function openPage(page_name) {
+    loadPage(page_name);
+
+    //add page history
+    window.history.pushState({page_name}, '', `/${page_name}`);
+}
+
+//backwards function
+window.onpopstate = function(ps) {
+    loadPage(ps.state.page_name);
 }
 
 
