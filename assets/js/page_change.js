d7856ef4741c5af6309f059fd8b091efaaaa7085
nikita-traynin <nikitatraynin@gmail.com>
2021-03-22 02:55:04 -0500

    finished converting hrefs to onclicks for SPA

--- assets/js/page_change.js
+++ assets/js/page_change.js
@@ -15,14 +15,35 @@ function openPage(page_name) {
     loadPage(page_name);
 
     //add page history
-    window.history.pushState({page_name}, '', `/${page_name}`);
+    window.history.pushState({type: "page", page_name: page_name}, '', `/${page_name}`);
 }
 
 //backwards function
 window.onpopstate = function(ps) {
-    loadPage(ps.state.page_name);
+    if (ps.state.type == "page") {
+        loadPage(ps.state.page_name);
+    }
+    else if (ps.state.type == "post") {
+        loadPost(ps.state.post_path);
+    }
 }
 
+function loadPost(post_path) {
+    $.get(post_path, function(data) {
+        //remove the previous content
+        $("#page-wrapper").remove();
+
+        //add the current content
+        $("nav").after(data);
+    })
+}
+
+function openPost(post_path) {
+    loadPost(post_path);
+
+    //add page history
+    window.history.pushState({type: "post", post_path: post_path}, '', post_path);
+}
 
 
 
